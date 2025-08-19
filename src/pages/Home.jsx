5ea042bb0ae42d58187f8346529c2820e7bc9127
Tushar-Singh07 <tusharsinghrajput06@gmail.com>
2025-08-19 18:25:18 +0530

    refactor: Update import paths for HomeBanner and Corosol components; remove unused HomePage function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,14 @@
 // src/pages/Home.jsx
 import React from "react";
-import HomeBanner from "./Page/HomeBanner";
-import Corosol from "./Page/Corosol";
+import HomeBanner from "./homebanner";
+import Corosol from "./Corosol";
 export default function Home() {
   return (
     <div className="container mx-auto pt-[100px] px-8">
       <h1 className="text-3xl font-bold mb-4">Welcome to Amrit Care!</h1>
       <p className="text-lg">Namste</p>
-    </div>
-  );
-}
-
-export function HomePage() {
-  return (
-    <div className="container mx-auto">
       <HomeBanner />
+      <Corosol />
     </div>
   );
 }
